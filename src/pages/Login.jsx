65da7fe5f554dd15b2b96d30bb42b6b6486a25a5
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-07-01 23:04:08 -0300

    Requisito 02: Finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,88 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameInputValue: '',
+      isButtonDisabled: true,
+      isLoading: false,
+    };
+  }
+
+  handleButton = () => {
+    const MINIMUM_CHARACTER = 3;
+    const { nameInputValue } = this.state;
+    const isDisable = nameInputValue.length < MINIMUM_CHARACTER;
+    this.setState({
+      isButtonDisabled: isDisable,
+    });
+  }
+
+  handleName = ({ target }) => {
+    this.setState({
+      nameInputValue: target.value,
+    }, this.handleButton);
+  }
+
+  switchIsLoading = () => {
+    const { isLoading } = this.state;
+    this.setState({
+      isLoading: !isLoading,
+    });
+  }
+
+  handleRedirect = () => {
+    const { history } = this.props;
+    history.push('/search');
+  }
+
+  handleSubmit = async () => {
+    try {
+      const { nameInputValue } = this.state;
+      this.switchIsLoading();
+      await createUser({ name: nameInputValue });
+      this.switchIsLoading();
+      this.handleRedirect();
+    } catch {
+      console.error('User not registered!');
+      this.switchIsLoading();
+    }
+  }
+
   render() {
-    return (
-      <div data-testid="page-login"> </div>
+    const { isButtonDisabled, isLoading } = this.state;
+    return isLoading ? <Loading /> : (
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            placeholder="Nome"
+            onChange={ this.handleName }
+            data-testid="login-name-input"
+          />
+          {/* <Link to="/search"> */}
+          <input
+            type="button"
+            disabled={ isButtonDisabled }
+            onClick={ this.handleSubmit }
+            data-testid="login-submit-button"
+            value="Entrar"
+          />
+          {/* </Link> */}
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;

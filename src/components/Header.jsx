996931798fed48ae4fd20839a8d9efe3b20660d7
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-07-02 23:00:48 -0300

    Requisito 04: Finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 // import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+
+import '../styles/header.css';
 
 class Header extends React.Component {
   constructor() {
@@ -13,6 +16,7 @@ class Header extends React.Component {
   }
 
   // ARRUMAR AQUI DEPOIS QUE TERMINAR
+  // BOTAR O getUser DENTRO DO IF
   async componentDidMount() {
     const KEY = 'currentUser';
     this.switchIsLoading();
@@ -33,13 +37,25 @@ class Header extends React.Component {
     });
   };
 
+  loadingElement = () => (
+    <span className="header-loading">
+      <Loading />
+    </span>
+  )
+
   render() {
     const { userName, isLoading } = this.state;
     return (
-      <header data-testid="header-component">
-        Header
+      <header className="header" data-testid="header-component">
+        <div>
+          <span>MyJam</span>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </div>
+
         <span data-testid="header-user-name">
-          { isLoading ? <Loading /> : userName }
+          { isLoading ? this.loadingElement() : userName }
         </span>
       </header>
     );

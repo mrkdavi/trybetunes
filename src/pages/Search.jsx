a2bcffa0ddf8c4163dfadc29866afb5baaf62bfc
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-07-02 23:55:46 -0300

    Requisito 05: Finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistNameInputValue: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  handleButton = () => {
+    const MINIMUM_CHARACTER = 2;
+    const { artistNameInputValue } = this.state;
+    const isDisable = artistNameInputValue.length < MINIMUM_CHARACTER;
+    this.setState({
+      isButtonDisabled: isDisable,
+    });
+  }
+
+  handleArtistName = ({ target }) => {
+    this.setState({
+      artistNameInputValue: target.value,
+    }, this.handleButton);
+  }
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search </div>
+        <div data-testid="page-search">
+          <input
+            type="search"
+            onChange={ this.handleArtistName }
+            data-testid="search-artist-input"
+            placeholder="Pesquisar artista"
+          />
+          <input
+            type="button"
+            disabled={ isButtonDisabled }
+            data-testid="search-artist-button"
+            value="Pesquisar"
+          />
+        </div>
       </>
     );
   }
